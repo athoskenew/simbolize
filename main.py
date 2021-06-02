c1614dfc41b4f335bbd8f8b952fae393b1ebcaf5
athoskenew <33551922+athoskenew@users.noreply.github.com>
2021-06-02 10:04:29 -0300

    melhorias no desempenho
    Agora a ferramenta demora menos tempo para completar todas as operações.

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import random
 time.sleep(2)
 
 
+#ajusta o layout para o layout padrao que o programa atua
+def ajustalayout():
+    pyautogui.click(545, 59) # seleciona caixa de zoom
+    pyautogui.click(506, 125) # ajusta ao tamanho padrao
+    pyautogui.click(25, 275) # seleciona ferramentas de desenho
+    pyautogui.click(81, 378) # clica na polilinhas (padrao)
+
 # cria duas coordenadas e clica no ponto dado pelas coordenadas
 def criarponto():
     y = random.randint(176, 227)
@@ -21,22 +28,12 @@ def coordenadas():
 
 # desenha o num de linhas especificados
 def desenharlinhas(num):
-    pyautogui.click(15, 269)  # clica na ferramenta polilinhas
     criarponto()  # cria o ponto inicial
     criarponto()  # cria segundo ponto da linha
     pyautogui.press('enter')  # encerra com enter
     ult_x, ult_y = pyautogui.position()
-    pyautogui.rightClick()
-    cx, cy = pyautogui.center(
-        pyautogui.locateOnScreen('copiar.png',
-                                 region=(452, 165, 402, 342)))  # vai ate a opcao copiar
-
-    pyautogui.click(cx, cy)  # clica na opcao copiar
-    pyautogui.rightClick()  # clica com o botao direito pra procurar a opcao colar
-    time.sleep(2)
-    print("achado em: ", pyautogui.center(pyautogui.locateOnScreen('colar.png')))
-    cox, coy = pyautogui.center(pyautogui.locateOnScreen('colar.png'))  # vai ate a opcao colar
-    pyautogui.click(cox, coy)  # clica na opcao colar
+    pyautogui.click(184, 58)  # clica na opcao copiar
+    pyautogui.click(211, 57)  # clica na opcao colar
     pyautogui.click(17, 135)   # ferramenta de selecao
     pyautogui.click(ult_x, ult_y)  # volta ao ultimo traco feito
     pyautogui.doubleClick(60, 87)  # clica pra alterar a coordanada e mover o traco
@@ -57,19 +54,9 @@ def desenharlinhas(num):
         pyautogui.click(codx, cody)  # clica no fim da linha anterior
         criarponto()  # cria nova linha a partir do fim da anterior
         pyautogui.press('enter')  # encerra nova linha com enter
-        time.sleep(1)  # aguarda 1s para pegar melhores valores do random
-
         ult_x, ult_y = pyautogui.position()
-        pyautogui.rightClick()
-        cx, cy = pyautogui.center(
-            pyautogui.locateOnScreen('copiar.png',
-                                     region=(452, 165, 402, 342)))  # vai ate a opcao copiar
-
-        pyautogui.click(cx, cy)  # clica na opcao copiar
-        pyautogui.rightClick()  # clica com o botao direito pra procurar a opcao colar
-        time.sleep(2)
-        cox, coy = pyautogui.center(pyautogui.locateOnScreen('colar.png'))  # vai ate a opcao colar
-        pyautogui.click(cox, coy)  # clica na opcao colar
+        pyautogui.click(184, 58)  # clica na opcao copiar
+        pyautogui.click(211, 57)  # clica na opcao colar
         pyautogui.click(17, 135)  # ferramenta de selecao
         pyautogui.click(ult_x, ult_y)  # volta ao ultimo traco feito
         pyautogui.doubleClick(60, 87)  # clica pra alterar a coordanada e mover o traco
@@ -78,6 +65,7 @@ def desenharlinhas(num):
         pyautogui.write(locais_tracos[a][1])  # move ate Xcm de altura da pagina
         pyautogui.click(15, 269)  # volta para a ferramenta de selecao
         pyautogui.moveTo(ult_x, ult_y)  # move de volta ao ultimo ponto criado
+
 
 # Escreve o texto passado por um arquivo txt nos campos requisitados
 def escrevertexto():
@@ -88,6 +76,7 @@ def escrevertexto():
         pyautogui.click(coord[i]) #clica na caixa de texto desejada
         pyautogui.write(texto)
 
+
 # Move o simbolo inicial criado completo para o fim da pagina
 def movesimbolo():
     pyautogui.click(17, 135)  # ferramenta de selecao
@@ -95,31 +84,19 @@ def movesimbolo():
     pyautogui.mouseDown()
     pyautogui.moveTo(553, 227)
     pyautogui.mouseUp()
-    pyautogui.click(515, 206,button='right')
-    time.sleep(2)
-    cx, cy = pyautogui.center(
-        pyautogui.locateOnScreen('copiar.png',
-                                 region=(452, 165, 402, 342)))  # vai ate a opcao copiar
-
-    pyautogui.click(cx, cy)  # clica na opcao copiar
-    pyautogui.click(1050, 385)  # oculta caixas de texto
-    pyautogui.moveTo(755,360)
-    pyautogui.rightClick()  # clica com o botao direito pra procurar a opcao colar
-    print("aqui")
-    time.sleep(2)
-    print("achado em: ", pyautogui.center(pyautogui.locateOnScreen('colar.png')))
-    cox, coy = pyautogui.center(pyautogui.locateOnScreen('colar.png'))  # vai ate a opcao colar
-    pyautogui.click(cox, coy)  # clica na opcao colar
-    pyautogui.doubleClick(60, 87)  # clica pra alterar a coordanada e mover o simbolo
+    pyautogui.click(184, 58)  # clica na opcao copiar
+    #pyautogui.moveTo(755,360)
+    pyautogui.click(211, 57)  # clica na opcao colar
+    pyautogui.doubleClick(60, 87)  # clica pra alterar a coordenada e mover o simbolo
     pyautogui.write('10,5')  # move ate Xcm da pagina
     pyautogui.doubleClick(64, 105)  # faz o mesmo com a altura
     pyautogui.write('1,97')  # move ate Xcm de altura da pagina
 
-
+ajustalayout()
+time.sleep(1)
 desenharlinhas(4)
 pyautogui.click(1048,385) # mostra as caixas de texto
 escrevertexto()
-time.sleep(20)
+time.sleep(18)
 movesimbolo()
-pyautogui.click(1047,457) # mostra as caixas de texto
 pyautogui.press('enter')  # encerra com enter
